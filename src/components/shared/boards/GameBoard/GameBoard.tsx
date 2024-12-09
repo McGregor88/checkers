ecf603f506328318eaf35204183ad6f08623edc3
Ruslan Igorevich <ruslan.igorevich@list.ru>
2024-12-09 21:25:54 +0300

    fixed timer cmp

--- src/components/shared/boards/GameBoard/GameBoard.tsx
+++ src/components/shared/boards/GameBoard/GameBoard.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 
 import './GameBoard.css';
 import { Player } from '../../../../models/Player';
+import Timer from './timer/Timer';
 
 interface BoardProps {
     currentPlayer: Player | null;
@@ -10,6 +11,7 @@ interface BoardProps {
 const GameBoard: FC<BoardProps> = ({ currentPlayer }) => (
     <div className="game-board">
         <div className="game-board-info">
+            <Timer currentPlayer={currentPlayer} />
             <span className="game-board-info__caption">Текущий игрок: </span>
             <b className="game-board-info__curr-player">{currentPlayer?.color}</b>
         </div>
--- src/components/shared/boards/GameBoard/timer/Timer.tsx
+++ src/components/shared/boards/GameBoard/timer/Timer.tsx
@@ -1,13 +1,51 @@
-import { FC } from 'react';
+import { FC, useState, useEffect, useRef } from 'react';
 import { Player } from '../../../../../models/Player';
+import { Colors } from '../../../../../models/Colors';
 
 interface TimerProps {
     currentPlayer: Player | null;
 }
 
+const TIME: number = 300;
+
 const Timer: FC<TimerProps> = ({ currentPlayer }) => {
+    const [blackTime, setBlackTime] = useState<number>(TIME);
+    const [whiteTime, setWhiteTime] = useState<number>(TIME);
+    const timer = useRef<null | ReturnType<typeof setInterval>>(null);
+
+    useEffect(() => {
+        startTimer();
+    }, [currentPlayer]);
+
+    function startTimer() {
+        if (timer.current) {
+            clearInterval(timer.current);
+        }
+
+        const callback = currentPlayer?.color === Colors.WHITE ? decrementWhiteTime : decrementBlackTime;
+
+        timer.current = setInterval(callback, 1000);
+    }
+
+    function decrementBlackTime() {
+        setBlackTime(prevTime => prevTime - 1);
+    }
+
+    function decrementWhiteTime() {
+        setWhiteTime(prevTime => prevTime - 1);
+    }
+
+    // TODO: при рестарте обнулить счетчик
+    const handleRestart = () => {
+        setBlackTime(TIME);
+        setWhiteTime(TIME);
+    }
+
     return (
-        <div>Timer</div>
+        <div>
+            <h2>Черные - {blackTime}</h2>
+            <h2>Белые - {whiteTime}</h2>
+        </div>
     );
 };
 
